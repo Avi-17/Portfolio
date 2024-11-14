b71c29dc1527430bb4e007d3bcf6fa5bf37119c1
Avi-17 <workhorseavi17@gmail.com>
2024-11-14 23:33:48 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         initial={{opacity: 0, x : 100}}
         transition={{duration:0.9}}
         className='m-8 flex items-center justify-center gap-4 text-2xl'>
-          <a href='../public/Resume.pdf' download="Resume.pdf" target='_blank' rel="noopener noreferrer"><IoIosDocument/></a>
+          <a href='../Resume.pdf' download="Resume.pdf" target='_blank' rel="noopener noreferrer"><IoIosDocument/></a>
           <a href='https://github.com/Avi-17' target='_blank' rel="noopener noreferrer"><FaGithub/></a>
           <a href='https://www.linkedin.com/in/aviral-singh-a01070287/' target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
           <a href='https://x.com/Aviral3108?t=IcBF2PPeJ95uY8ZQlsVY1w&s=09' target='_blank' rel="noopener noreferrer"><FaSquareXTwitter/></a>
